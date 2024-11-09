63ea4486a17c3e7c557568b6aebed5650b9a0327
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2024-11-08 20:19:56 -0500

    make game open

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+# import statements
 import pygame
+from constants import *
+
+
+def game_loop(screen):
+    while True:
+        # check if quit
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+    pygame.display.flip()
 
 
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+
+    # create gui window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # start game
+    game_loop(screen)
 
 
 if __name__ == "__main__":
