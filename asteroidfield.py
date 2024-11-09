802141ee248b8b76338bc1200fefaa3c3f5f9a37
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2024-11-09 16:35:45 -0500

    fix asteroid spawn rate

--- asteroidfield.py
+++ asteroidfield.py
@@ -41,11 +41,11 @@ class AsteroidField(pygame.sprite.Sprite):
         if self.spawn_timer > ASTEROID_SPAWN_RATE:
             self.spawn_timer = 0
 
-        # spawn a new asteroid at a random edge
-        edge = random.choice(self.edges)
-        speed = random.randint(40, 100)
-        velocity = edge[0] * speed
-        velocity = velocity.rotate(random.randint(-30, 30))
-        position = edge[1](random.uniform(0, 1))
-        kind = random.randint(1, ASTEROID_KINDS)
-        self.spawn(ASTEROID_MIN_RADIUS * kind, position, velocity)
+            # spawn a new asteroid at a random edge
+            edge = random.choice(self.edges)
+            speed = random.randint(40, 100)
+            velocity = edge[0] * speed
+            velocity = velocity.rotate(random.randint(-30, 30))
+            position = edge[1](random.uniform(0, 1))
+            kind = random.randint(1, ASTEROID_KINDS)
+            self.spawn(ASTEROID_MIN_RADIUS * kind, position, velocity)
